dbd0c8f744af83268312ffbf85b0ecdcf18a3c55
aldosaucedo1213 <a.22308051281310@cbtis128.edu.mx>
2025-02-11 16:53:55 +0000

    hola lista

---
+++ lista1.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Listas tipo entero de 3 elementos
+  List<int> numeros = [10, 20, 30];
+  print("lista de numeros enteros: ");
+  print(numeros);
+  print("primer elemento: ${numeros[0]}");
+  print("segundo elemento: ${numeros[1]}");
+  print("tercer elemento: ${numeros[2]}");
+  print("elementos de la lista usando ciclo for: ");
+  for (int i = 0; i < numeros.length; i++) {
+    print(numeros[i]);
+  }
+
+  
+}  

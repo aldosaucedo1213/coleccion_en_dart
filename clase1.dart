852da85c4d20ce11919956cf5dc32bee5283515b
aldosaucedo1213 <a.22308051281310@cbtis128.edu.mx>
2025-02-13 19:12:17 +0000

    ejemplo clase1

---
+++ clase1.dart
@@ -0,0 +1,25 @@
+class figura {
+  int _largo;
+  int _ancho;
+//Constructor
+  figura(this._largo, this._ancho);
+
+  void mostrar(){
+    print("Largo: $_largo");
+    print("Ancho: $_ancho");
+    } //funcion mostrar
+    void calcularArea(){
+      print("Area: ${_largo * _ancho}");
+    }
+      void calcularPerimetro(){
+        print("Perimetro: ${2 * (_largo + _ancho)}");
+       } //funcion calcularPerimetro 
+  } //clase figura
+    void main (){
+      print("aldo daniel saucedo de luna mat 22308051281310 gpo 6 j");
+      var rectangulo = figura(10, 5);
+      rectangulo.mostrar();
+      rectangulo.calcularArea();
+      rectangulo.calcularPerimetro();
+    } //fin de main 
+
---
+++ recursos/clase1.md
@@ -0,0 +1,3 @@
+salida de la clase figura objeto rectangulo
+
+![alt text](image-4.png)
